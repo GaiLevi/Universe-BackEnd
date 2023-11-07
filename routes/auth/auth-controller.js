fca2ce9c1797c274fa4564e9549a7a6e17201819
Nadav Vazana <nadavvivsa@gmail.com>
2023-11-08 00:01:55 +0200

    sadas

--- routes/auth/auth-controller.js
+++ routes/auth/auth-controller.js
@@ -5,7 +5,12 @@ async function login(req, res) {
     const user = await authService.login(req.body);
     const loginToken = authService.getLoginToken(user._id);
     console.log(loginToken);
-    res.cookie("loginToken", loginToken, { httpOnly: true, secure: true });
+    res.cookie("loginToken", loginToken, {
+      httpOnly: true,
+      secure: true,
+      domain: "https://universe-frontend.onrender.com",
+      path: "/",
+    });
     res.send(user);
   } catch (error) {
     res.status(error.status).send(error);
